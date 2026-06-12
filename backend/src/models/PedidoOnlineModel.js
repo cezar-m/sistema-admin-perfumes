89b1356c8fdbec1139da9db725fe0c3261a9dea4
cezar-m <103004018+cezar-m@users.noreply.github.com>
2026-06-12 13:06:26 -0300

    Update PedidoOnlineModel.js

--- backend/src/models/PedidoOnlineModel.js
+++ backend/src/models/PedidoOnlineModel.js
@@ -154,14 +154,12 @@ class PedidoOnlineModel {
 			
 			// Atualiza status e registra o aprovador
 			await client.execute(
-				await db.query(
 			    `UPDATE pedidos_online
 			     SET status = 'aprovado',
 			         aprovado_por_id = $1
 			     WHERE id = $2`,
 			    [aprovadorId, pedidoId]
 			);
-		   )
 			
 			await client.query('COMMIT');
 			console.log(`Pedido ${pedidoId} aprovado por usuário ${aprovadoPorId}`);
