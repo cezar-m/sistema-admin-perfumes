7a907463cdb80edbd68e25c976abbac287ae4c2b
cezar-m <103004018+cezar-m@users.noreply.github.com>
2026-06-12 02:20:43 -0300

    Update database.js

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,23 +1,11 @@
-const mysql = require('mysql2/promise');
+const { Pool } = require('pg');
 require('dotenv').config();
 
-let poolConfig;
-
-if (process.env.DATABASE_URL) {
-  // Produção (Render)
-  poolConfig = process.env.DATABASE_URL;
-} else {
-  // Desenvolvimento local
-  poolConfig = {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    waitForConnections: true,
-    connectionLimit: 10,
-  };
-}
-
-const pool = mysql.createPool(poolConfig);
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 module.exports = pool;
