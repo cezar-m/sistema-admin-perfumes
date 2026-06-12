38b7a4bde50d2b175ca8e645eec283b90d97f7ec
cezar-m <103004018+cezar-m@users.noreply.github.com>
2026-06-12 02:02:51 -0300

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,22 +1,27 @@
 import axios from 'axios';
 
 const api = axios.create({
-	baseURL: 'http://localhost:5001/api'
+	baseURL: import.meta.env.VITE_API_URL
 });
 
-api.interceptors.request.use(config => {
+api.interceptors.request.use((config) => {
 	const token = localStorage.getItem('token');
-	if(token) config.headers.Authorization = `Bearer ${token}`;
+
+	if (token) {
+		config.headers.Authorization = `Bearer ${token}`;
+	}
+
 	return config;
 });
 
 api.interceptors.response.use(
-	response => response,
-	error => {
-		if(error.response?.status === 401) {
+	(response) => response,
+	(error) => {
+		if (error.response?.status === 401) {
 			localStorage.clear();
 			window.location.href = '/login';
 		}
+
 		return Promise.reject(error);
 	}
 );
