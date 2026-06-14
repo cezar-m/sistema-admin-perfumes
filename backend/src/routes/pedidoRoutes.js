a74643bd979313b1b71c744e9d04281bd9477735
cezar-m <103004018+cezar-m@users.noreply.github.com>
2026-06-14 14:54:13 -0300

    Update pedidoRoutes.js

--- backend/src/routes/pedidoRoutes.js
+++ backend/src/routes/pedidoRoutes.js
@@ -2,8 +2,9 @@ const router = require('express').Router();
 const PedidoController = require('../controllers/PedidoOnlineController');
 const { authenticate } = require('../middlewares/authMiddleware');
 
+// Aplica authenticate em todas as rotas
 router.post('/', authenticate, PedidoController.criarPedido);
-router.get('/meus-pedidos' , authenticate, PedidoController.listarMeusPedidos);
+router.get('/meus-pedidos', authenticate, PedidoController.listarMeusPedidos);
 router.get('/', authenticate, PedidoController.listarTodosPedidos);
 router.put('/:id/aprovar', authenticate, PedidoController.aprovarPedido);
 
